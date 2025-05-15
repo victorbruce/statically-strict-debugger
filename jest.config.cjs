999e20472112cd5230aba0a6d909dc63040939f6
Victor Bruce <victorbruce82@gmail.com>
2025-05-15 16:58:29 +0000

    test: setup testing environment

---
+++ jest.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/tests/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.json'
+    }]
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'src'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/index.ts",
+    "!src/**/*.d.ts"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+}; 
